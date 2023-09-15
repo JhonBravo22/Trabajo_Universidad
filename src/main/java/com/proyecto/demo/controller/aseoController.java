40387dc9613586cfae665bf681f4eb3a70a93abd
JhonBravo22 <bravojhon66@gmail.com>
2023-09-15 15:48:59 -0500

    configuraciones varias

--- src/main/java/com/proyecto/demo/controller/aseoController.java
+++ src/main/java/com/proyecto/demo/controller/aseoController.java
@@ -1,12 +1,21 @@
 package com.proyecto.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class aseoController {
+
+    @GetMapping("/createAseo/{id}")
+        public String createAseo(@PathVariable("id") String id){
+        String regexforID = "[A-Z 0-9 a-z]{0,15}";
+            if (id == null || !id.matches(regexforID))
+            throw new IllegalArgumentException();
+         return "Aseo creado satisfactoriamente "+id;
+        }
 
     @GetMapping("/createAseo")
     public String createAseo (){
