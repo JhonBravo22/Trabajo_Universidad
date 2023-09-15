b8234a134cddd36cb77f108f9d3006471477b140
JhonBravo22 <bravojhon66@gmail.com>
2023-09-15 15:47:49 -0500

    configuraciones

--- src/main/java/com/proyecto/demo/controller/juguetesController.java
+++ src/main/java/com/proyecto/demo/controller/juguetesController.java
@@ -1,12 +1,21 @@
 package com.proyecto.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class juguetesController {
+
+    @GetMapping("/createjuguetes/{id}")
+        public String createJuguetes(@PathVariable("id") String id){
+        String regexforID = "[A-Z 0-9 a-z]{0,15}";
+            if (id == null || !id.matches(regexforID))
+            throw new IllegalArgumentException();
+         return "Juguetes creado satisfactoriamente "+id;
+        }
 
     @GetMapping("/createJuguetes")
     public String createJuguetes (){
